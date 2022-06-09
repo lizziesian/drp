4d09535519ace1204a4f18d553058f350e25cd8c
rosie <72262688+rosie-jlh@users.noreply.github.com>
2022-06-09 22:15:25 +0100

    Removed all database code.

--- exerciseapp/app.py
+++ exerciseapp/app.py
@@ -2,28 +2,29 @@
 from flask import Flask, render_template, request
 #import xml_lib
 
-from database import database
-from models.user import User
+#from .database import database
+#from .models.user import User
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
+#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-database.init_app(app)
+#database.init_app(app)
 
-from .cli import create_all, drop_all, populate
+#from .cli import create_all, drop_all, populate
 
-with app.app_context():
-    app.cli.add_command(create_all)
-    app.cli.add_command(drop_all)
-    app.cli.add_command(populate)
+#with app.app_context():
+#    app.cli.add_command(create_all)
+#    app.cli.add_command(drop_all)
+#    app.cli.add_command(populate)
 
 @app.route("/")
 @app.route("/mission_start")
 def mission_start():
-    the_user = User.query.get_or_404(0, "User not found.")
-    if the_user:
-        return render_template("mission_start.html", title="Mission Start", user=the_user)
+    #the_user = User.query.get_or_404(0, "User not found.")
+    #if the_user:
+    #    return render_template("mission_start.html", title="Mission Start", user=the_user)
+    return render_template("mission_start.html", title="Mission Start")
 
 @app.route("/exercise_video")
 def exercise_video():
@@ -31,11 +32,12 @@ def exercise_video():
 
 @app.route("/mission_complete")
 def mission_complete():
-    the_user = User.query.get_or_404(0, "User not found.")
-    if the_user:
-        the_user.level += 1
-        database.session.commit()
-        return render_template("mission_complete.html", title="Mission Complete", user=the_user)
+    #the_user = User.query.get_or_404(0, "User not found.")
+    #if the_user:
+    #    the_user.level += 1
+    #    database.session.commit()
+    #    return render_template("mission_complete.html", title="Mission Complete", user=the_user)
+    return render_template("mission_complete.html", title="Mission Complete")
 
 ''' @app.route('/movieList')
 def movie_list():
