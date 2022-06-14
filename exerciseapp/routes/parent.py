55e9202f4be236396a040da351f52a7f4f485e74
rosie <72262688+rosie-jlh@users.noreply.github.com>
2022-06-14 22:52:31 +0100

    Created account selection page to choose child account or parent account. Upon choice redirects to parent home page or child home page.

--- exerciseapp/routes/parent.py
+++ exerciseapp/routes/parent.py
@@ -1,4 +1,3 @@
-import re
 from flask import Blueprint, render_template
 
 parent = Blueprint("parent", __name__, url_prefix="/parent")
@@ -6,4 +5,4 @@ parent = Blueprint("parent", __name__, url_prefix="/parent")
 @parent.route("/")
 @parent.route("/home")
 def home():
-    return render_template("home_parent", title="Home")
+    return render_template("home_parent.html", title="Home")
