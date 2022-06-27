661098f049cca1c620905803caf92d67ee7879fd
rosie <72262688+rosie-jlh@users.noreply.github.com>
2022-06-27 13:29:26 +0100

    Fixed typo in init file.

--- exerciseapp/__init__.py
+++ exerciseapp/__init__.py
@@ -7,7 +7,7 @@ from flask_socketio import SocketIO
 app = Flask(__name__, static_url_path="/exerciseapp/static")
 
 # sqlalchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = ('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///user.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
